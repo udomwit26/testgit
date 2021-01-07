36684ccc390cbf12230d54ac69c7454d7f76b153
udomwit <udomwit@gmail.com>
2021-01-07 16:08:13 +0700

    fix bug

--- hello.go
+++ hello.go
@@ -4,19 +4,4 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello world")
-	fmt.Println("main1")
-	fmt.Println("xxxxyyyy")
-	fmt.Println("zzzzzz")
-	fmt.Println("main2")
-	fmt.Println("new line")
-	fmt.Println("new line2")
-	fmt.Println("new line3")
-	fmt.Println("new line4")
-	fmt.Println("new line6")
-	fmt.Println("new line6.1")
-	fmt.Println("new line5")
-	fmt.Println("new line7")
-	fmt.Println("new line8")
-	fmt.Println("new line9")
-	//test
 }
