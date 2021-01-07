e0134d8c95984a22d64064d229139b3e9e82bfaf
udomwit <udomwit@gmail.com>
2021-01-07 15:03:33 +0700

    add local

--- hello.go
+++ hello.go
@@ -10,10 +10,12 @@ func main() {
 	fmt.Println("new line")
 	fmt.Println("new line2")
 	fmt.Println("new line3")
+	fmt.Println("new line3.2")
 	fmt.Println("new line4")
 	fmt.Println("new line6")
 	fmt.Println("new line5")
 	fmt.Println("new line7")
+	fmt.Println("new line7.2")
  fmt.Println("new line8")
 
 }
